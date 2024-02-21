c22cbd086a28b5f8208d75884c63cb7419b09245
robertkoder <robertk.koder@gmail.com>
2024-02-21 10:00:46 +0100

    init

---
+++ main.js
@@ -0,0 +1,5 @@
+import * as search from "./scripts/search.js";
+import * as template from "./scripts/template.js";
+import * as randomizer from "./scripts/randomizer.js";
+
+// API: https://www.tvmaze.com/api
